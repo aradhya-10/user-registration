3ad6cdb18c1983a464a1aa51984b8a7fc67e1148
aradhya-10 <202051034@iiitvadodara.ac.in>
2023-12-13 18:43:22 +0530

    UC3: Validate user's email

--- src/main/java/org/example/UserRegistration.java
+++ src/main/java/org/example/UserRegistration.java
@@ -12,7 +12,7 @@ public class UserRegistration {
 
         // Validate the first name
         while(!isValidName(firstName)) {
-            System.out.println("Invalid first name! Please enter a first name starting with a capital letter and at least 3 characters long.");
+            System.out.println("Invalid first name! Must start with a capital letter and be at least 3 characters long.");
             firstName = sc.nextLine();
         }
         System.out.println("Valid first name entered: " + firstName);
@@ -23,10 +23,21 @@ public class UserRegistration {
 
         // Validate the first name
         while(!isValidName(lastName)) {
-            System.out.println("Invalid last name! Please enter a last name starting with a capital letter and at least 3 characters long.");
+            System.out.println("Invalid last name! Must start with a capital letter and be at least 3 characters long.");
             lastName = sc.nextLine();
         }
         System.out.println("Valid last name entered: " + lastName);
+
+        // Ask the user to enter the last name
+        System.out.println("Enter your email: ");
+        String email = sc.nextLine();
+
+        // Validate the first name
+        while(!isValidName(email)) {
+            System.out.println("Invalid email! Please enter a valid email.");
+            email = sc.nextLine();
+        }
+        System.out.println("Valid email entered: " + email);
     }
 
     // UC1 & UC2: Validate the first and last names
@@ -34,6 +45,13 @@ public class UserRegistration {
     //    return firstName.length() >= 3 && Character.isUpperCase(firstName.charAt(0));
         String nameRegex = "^[A-Z][a-zA-Z]{2,}$";
         return name.matches(nameRegex);
+    }
+
+    // UC3: Validate the email
+    public static boolean isValidEmail(String email) {
+        // Email pattern with 3 mandatory parts and 2 optional parts with precise @ and . positions
+        String emailRegex = "^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+(\\.[a-zA-Z]{2,}){1,2}$";
+        return email.matches(emailRegex);
     }
 }
 
